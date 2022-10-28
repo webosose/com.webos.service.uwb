567eddcf4d543212d477032dd2c921ecaef2a21f
deepan.shivap <deepan.shivap@lge.com>
2022-10-28 11:41:48 +0530

    To Uncomment the buildSpecificInfo and buildServiceState function declarations.
    :Release Notes:
    Implemented WRO-14871
    [UWB] To Deprecate getUwbServiceState and getUwbSpecificInfo API
    
    :Detailed Notes:
    Uncommented the buildSpecificInfo and buildServiceState function
    declarations.
    
    :Testing Performed:
    tested locally
    
    :QA Notes:
     NA
    
    :Issues Addressed:
    [WR0-14871] - [UWB] To Deprecate getUwbServiceState and getUwbSpecificInfo API
    
    Change-Id: Iba8f3448f85b64a1039dfdf7129a0293eae5dfb4
    Reviewed-on: http://gpro.lge.com/c/webosose/com.webos.service.uwb/+/338101
    Reviewed-by: Commit Msg Checker <commit_msg@lge.com>
    Reviewed-by: Ban Word Checker <ban_word@lge.com>
    Reviewed-by: <muralidhar.n@lge.com>
    Tested-by: <muralidhar.n@lge.com>
    (cherry picked from commit 6aa06d415ef408a608be3aeeeb9f10a658f50f75)
    Reviewed-on: http://gpro.lge.com/c/webosose/com.webos.service.uwb/+/337689

--- src/UwbResponseBuilder.h
+++ src/UwbResponseBuilder.h
@@ -18,8 +18,8 @@
 class UwbResponseBuilder : public IResponseBuilder {
 private:
     void buildRangingInfo(pbnjson::JValue &responseObj, std::unique_ptr<UwbRangingInfo>& rangingInfo) override;
-  //  void buildSpecificInfo(pbnjson::JValue &responseObj, UwbSpecInfo &info) override;
-  //  void buildServiceState(pbnjson::JValue &responseObj, bool isServiceAvailable) override;
+    void buildSpecificInfo(pbnjson::JValue &responseObj, UwbSpecInfo &info) override;
+    void buildServiceState(pbnjson::JValue &responseObj, bool isServiceAvailable) override;
 };
 
 
